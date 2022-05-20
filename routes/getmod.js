498fc5a1843f7491ce07503b4bc46df70335ee7f
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-05-20 22:27:03 +0200

    Add mockup getmod request handler

---
+++ routes/getmod.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET mod info. */
+router.get('/', function(req, res, next) {
+  res.json({
+    name: 'Test Mod',
+    image: 'http://140.238.75.242:3003/images/testmodimage.png',
+    id: 0,
+    description: 'This is a description',
+    author: 0,
+    versionbucket: 0
+  });
+});
+
+module.exports = router;
+module.exports.routePath = '/getmod'
